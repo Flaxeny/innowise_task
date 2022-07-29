e0c33ce388afd468da4f967e05ef67008f353b38
Flaxen <romskor11@gmail.com>
2022-07-29 18:12:35 +0300

    11-fix serializers

--- chat/serializers.py
+++ chat/serializers.py
@@ -16,7 +16,7 @@ class UserSerializer(serializers.ModelSerializer):
         def create(self, validated_data):
             password = validated_data.pop('password', None)
             instance = self.Meta.model(**validated_data)
-            if password is not None:
+            if password:
                 instance.set_password(password)
             instance.save()
             return instance
